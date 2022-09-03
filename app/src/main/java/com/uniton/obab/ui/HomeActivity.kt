2ed5fe5bd6fb6b5253856fbb95ff41b3cabcb86d
김태웅 <rlaxodnd95@gmail.com>
2022-09-04 03:41:33 +0900

    :lipstick: 투표 진행 중 뒤로가기 클릭 시 취소 방지 다이얼로그 제작

--- app/src/main/java/com/uniton/obab/ui/HomeActivity.kt
+++ app/src/main/java/com/uniton/obab/ui/HomeActivity.kt
@@ -2,11 +2,14 @@ package com.uniton.obab.ui
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.uniton.obab.databinding.ActivityHomeBinding
 
 class HomeActivity : AppCompatActivity() {
     private lateinit var binding: ActivityHomeBinding
+    private val finishTime: Long = 1000
+    private var pressTime: Long = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityHomeBinding.inflate(layoutInflater)
@@ -22,7 +25,6 @@ class HomeActivity : AppCompatActivity() {
 
     }
 
-
     private fun changeEnterRoomActivity() {
         val intent = Intent(this, EnterRoomActivity::class.java)
         intent.addFlags(Intent.FLAG_ACTIVITY_NEW_DOCUMENT)
@@ -32,6 +34,18 @@ class HomeActivity : AppCompatActivity() {
     private fun changeCreateRoomActivity() {
         val intent = Intent(this, CreateRoomActivity::class.java)
         startActivity(intent)
+    }
+
+    override fun onBackPressed() {
+        val tempTime = System.currentTimeMillis()
+        val intervalTime: Long = tempTime - pressTime
+
+        if (intervalTime in 0..finishTime) {
+            finish()
+        } else {
+            pressTime = tempTime
+            Toast.makeText(applicationContext, "한번더 누르시면 앱이 종료됩니다", Toast.LENGTH_SHORT).show()
+        }
     }
 
 }
--- app/src/main/java/com/uniton/obab/ui/ResultActivity.kt
+++ app/src/main/java/com/uniton/obab/ui/ResultActivity.kt
@@ -3,10 +3,22 @@ package com.uniton.obab.ui
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.uniton.obab.R
+import com.uniton.obab.databinding.ActivityHomeBinding
+import com.uniton.obab.databinding.ActivityResultBinding
 
 class ResultActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityResultBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_result)
+        binding = ActivityResultBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        initListener()
+    }
+
+    private fun initListener() = with(binding) {
+        btnDone.setOnClickListener {
+            finish()
+        }
     }
 }
