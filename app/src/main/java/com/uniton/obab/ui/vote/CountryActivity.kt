a8978ce24689c61e5f95ee1e612410a389ea6c6e
Jihee.Han <42907876+jihee-dev@users.noreply.github.com>
2022-09-03 16:02:41 +0900

    :lipstick: 메뉴 투표 화면 구현

---
+++ app/src/main/java/com/uniton/obab/ui/vote/CountryActivity.kt
@@ -0,0 +1,12 @@
+package com.uniton.obab.ui.vote
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.uniton.obab.R
+
+class CountryActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_country)
+    }
+}
